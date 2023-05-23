2034416df4155f8510ae60b982eded83f9fa0023
piojank <p.jankowski931026@gmail.com>
2023-05-23 19:37:39 +0200

    add RandomHexColor

--- src/components/stats/item.jsx
+++ src/components/stats/item.jsx
@@ -1,10 +1,21 @@
 import styles from "./Statistics.module.css";
 
+const getRandomHexColor = () => {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, '0')}`;
+};
+
 const Item = ({ id, label, percentage }) => {
     return (
-        <li className={styles.item}>
-        <span className={styles.label}>{label}</span>
-        <span className={styles.percentage}>{percentage}%</span>
+        <li 
+            className={styles.item}
+            style={{
+                backgroundColor: getRandomHexColor(),
+            }}
+            >
+            <span className={styles.label}>{label}</span>
+            <span className={styles.percentage}>{percentage}%</span>
         </li>
     );
 }
