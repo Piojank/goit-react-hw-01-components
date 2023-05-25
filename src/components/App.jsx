34b3e76e2cad8675fc1bf5df4ad9b608e7f898a8
piojank <p.jankowski931026@gmail.com>
2023-05-25 18:48:03 +0200

    fix folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,7 @@
-import "../index.css";
-import { Profile }  from "./Profile/Profile";
-import { Statistics } from "./Stats/Statistics";
-import { FriendList } from "./Friends/FriendList";
-import { TransactionHistory } from "./Transaction/TransactionHistory";
+import { Profile }  from "./rofile/Profile";
+import { Statistics } from "./tats/Statistics";
+import { FriendList } from "./riends/FriendList";
+import { TransactionHistory } from "./ransaction/TransactionHistory";
 
 import user from "../data/user.json";
 import stats from "../data/stats.json";
